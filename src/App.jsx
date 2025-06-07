5c56e1ebe9e9bc27c83df1b017266d268b7065f1
Bryan Garay <bryangarayacademico@gmail.com>
2025-06-07 13:18:55 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+import { Destop1440Px } from "./Destop1440Px/Destop1440Px";
+
+export default function App() {
+  return (
+    <div>
+      <Destop1440Px />
+    </div>
+  );
+}
